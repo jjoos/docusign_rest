41578dde7ad77e73c7a2f10c63fee00d51676ae5
Steven Shelby <steve@shx.ca>
2013-05-25 11:16:56 -0400

    fixed json in add_envelope_recipients

--- lib/docusign_rest/client.rb
+++ lib/docusign_rest/client.rb
@@ -621,7 +621,8 @@ module DocusignRest
       uri = build_uri("/accounts/#{@acct_id}/envelopes/#{options[:envelope_id]}/recipients")
       
       post_body = "{ \"signers\": #{options[:recipients].to_json} }"
-
+      puts uri
+      puts post_body
       http = initialize_net_http_ssl(uri)
       request = Net::HTTP::Post.new(uri.request_uri, headers(content_type))
       request.body = post_body
