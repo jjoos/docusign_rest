ff74aaece55096bac31bcf8980195e83387e20fd
Ben VandenBos <ben@frontdeskhq.com>
2015-04-14 08:47:57 -0700

    fixing monkey patch

--- lib/multipart_post/parts.rb
+++ lib/multipart_post/parts.rb
@@ -2,7 +2,7 @@ require 'multipart_post'
 require 'parts'
 
 Parts::ParamPart.class_eval do
-  def build_part(boundary, name, value)
+  def build_part(boundary, name, value, headers = {})
     part = "\r\n" #Add a leading carriage return line feed (not sure why DocuSign requires this)
     part << "--#{boundary}\r\n"
     part << "Content-Type: application/json\r\n" #Add the content type which isn't present in the multipart-post gem, but DocuSign requires
